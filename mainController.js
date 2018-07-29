6d76368cfebca38ccd55bea7b7a23ebdc8d85cae
getsong <getsong13@gmail.com>
2018-07-29 13:44:43 +0800

    populate table time and table cells, add table border style

--- mainController.js
+++ mainController.js
@@ -2,17 +2,16 @@
 
     var app = angular.module("meetingScheduler");
 
-    var mainController = function ($scope, $logger) {
+    var mainController = function ($scope, $log) {
         $scope.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        $scope.startTime = new Date(0, 0, 0, 8, 00);
-        $scope.endTime = new Date(0, 0, 0, 5, 00);
-        $scope.timeArr = []
+        $scope.startTime = new Date(0, 0, 0, 8);
+        $scope.endTime = new Date(0, 0, 0, 17);
+        $scope.startTimeArr = []
         for (var time=$scope.startTime; time<$scope.endTime; time=new Date(time.getTime() + 30 * 60 * 1000)) {
-            $scope.timeArr.push(time);
-            $logger(time);
+            $scope.startTimeArr.push(time);
         }
-        console.log($scope.timeArr);
-        // $scope.time = 
+        $scope.endTimeArr = $scope.startTimeArr.slice(1);
+        $scope.endTimeArr.push($scope.endTime);
     }
 
     app.controller("MainController", mainController);
