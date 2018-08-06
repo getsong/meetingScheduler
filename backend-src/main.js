effa44be92f287bf13aa1f7f9ebb5e8be0957886
getsong <getsong13@gmail.com>
2018-08-06 22:51:32 +0800

    backend create event post api

--- backend-src/main.js
+++ backend-src/main.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require('express')
+const app = express();
 const mysql = require('mysql');
 const parser = require('body-parser');
 
-const app = express();
 app.use(parser.json());
 
 var connection = mysql.createConnection({
@@ -12,22 +12,41 @@ var connection = mysql.createConnection({
   database: 'meetingscheduler'
 });
 
-// insert statement
-let stmt = `INSERT INTO daily_records(date,0t)  VALUES ?  `;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+// create event
+let eventInsertion = `INSERT INTO events(EVENT_NAME,SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY) VALUES ?  `;
+
+app.post('/event', (req, res) => {
+  console.log(req.body);
+
+  // execute the insert statment
+  connection.query(eventInsertion, [req.body], (err, results) => {
+    if (err) {
+      console.error(err.message);
+      res.send(err.message);
+      return;
+    }
+    // get inserted rows
+    console.log('Row inserted:' + results.affectedRows);
+    res.send('Row inserted:' + results.affectedRows);
+  });
+});
+
+// create record
+let recordInsertion = `INSERT INTO daily_records(date,0t)  VALUES ?  `;
 let todos = [
   ['2018-05-08', false],
   ['2018-05-09', true]
 ];
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.post('/', (req, res) => {
+app.post('/record', (req, res) => {
   console.log(req.body);
 
   // execute the insert statment
-  connection.query(stmt, [req.body], (err, results) => {
+  connection.query(recordInsertion, [req.body], (err, results) => {
     if (err) {
       console.error(err.message);
       res.send("error!");
