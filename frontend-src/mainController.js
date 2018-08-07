4efe134ddb2bafc315852bde97cb8f3d62bfd32d
getsong <getsong13@gmail.com>
2018-08-07 21:42:59 +0800

    event creation click to select and unselect

--- frontend-src/mainController.js
+++ frontend-src/mainController.js
@@ -3,7 +3,33 @@
     var app = angular.module("meetingScheduler");
 
     var mainController = function ($scope) {
+        $scope.eventName;
         $scope.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        $scope.selectedDays = []
+        $scope.isMouseDown = false;
+        for (var i = 0; i < 7; i++) {
+            $scope.selectedDays.push(false);
+        }
+
+        $scope.mouseDown = function (index) {
+            $scope.isMouseDown = true;
+            $scope.selectedDays[index] = !$scope.selectedDays[index];
+        };
+
+        $scope.mouseEnter = function (index) {
+            if ($scope.isMouseDown) {
+                $scope.selectedDays[index] = !$scope.selectedDays[index];
+            }
+        };
+
+        $scope.mouseUp = function () {
+            $scope.isMouseDown = false;
+            console.log($scope.selectedDays);
+        };
+
+        // $scope.createEvent = function () {
+        //     $http.post()
+        // }
     };
 
     app.controller("MainController", mainController);
