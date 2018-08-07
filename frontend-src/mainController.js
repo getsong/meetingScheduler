057d422001e643e8be769ed201daaa6f7347e41f
getsong <getsong13@gmail.com>
2018-08-07 20:32:56 +0800

    add days array and styling

--- frontend-src/mainController.js
+++ frontend-src/mainController.js
@@ -3,7 +3,7 @@
     var app = angular.module("meetingScheduler");
 
     var mainController = function ($scope) {
-
+        $scope.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     };
 
     app.controller("MainController", mainController);
