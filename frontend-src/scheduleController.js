d4a7a597ea452760accbb4dbc2d1f48db855038b
Unknown <getsong13@gmail.com>
2018-08-08 22:43:46 +0800

    minor fix: end time 17 -> 18

--- frontend-src/scheduleController.js
+++ frontend-src/scheduleController.js
@@ -5,7 +5,7 @@
     var schedulerController = function ($scope) {
         $scope.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         $scope.startTime = new Date(0, 0, 0, 8);
-        $scope.endTime = new Date(0, 0, 0, 17);
+        $scope.endTime = new Date(0, 0, 0, 18);
         $scope.startTimeArr = [];
         for (var time = $scope.startTime; time < $scope.endTime; time = new Date(time.getTime() + 30 * 60 * 1000)) {
             $scope.startTimeArr.push(time);
